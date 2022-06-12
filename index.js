dc74680ccd5b45ddacb48644c0bc565782af3e8e
kevin <kmunene858@gmail.com>
2022-06-12 15:55:44 +0300

    commit tree 2b84df2df8326d524cf37cbd0c78da00c73c3fc9

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map((a) => {
+    let x = a.split(' ')
+    let cap = x.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    
+    let y = cap.join(' ')
+    return y
+  })
+
 }
